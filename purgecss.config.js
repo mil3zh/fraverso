918ee9d6bcde8754ee1f6e84c2be7b9eaaddca54
mil3zh <milena.lazzaretti5@gmail.com>
2020-08-23 10:23:59 +0200

    node modules

---
+++ purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // These are the files that Purgecss will search through
+  content: ["./_site/**/*.html"],
+  // These are the stylesheets that will be subjected to the purge
+  css: ["./_site/css/styles.css"]
+};
